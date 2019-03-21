110a42005b92c4c3505e0fa205f762783c14e875
Hoon Yang <ssang276@hanmail.net>
2019-03-21 14:08:37 +0900

    Support Maven

--- src/test/java/zcp/demo/alert/DemoAlertApplicationTests.java
+++ src/test/java/zcp/demo/alert/DemoAlertApplicationTests.java
@@ -1,4 +1,4 @@
-package zcp.demo;
+package zcp.demo.alert;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class DemoAlertApplicationTests {
 
 	@Test
 	public void contextLoads() {
